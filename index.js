7db653c3e5966cb223fe52cdbf755bd0a6f9600f
Valentin Wetenkamp <valentin.wetenkamp@googlemail.com>
2015-08-17 17:07:39 +0200

    clean up, this build of browserify was a bit old..

--- index.js
+++ index.js
@@ -6,23 +6,15 @@ var plugins = {
   babelify: require('babelify'),
   gutil: require('gulp-util'),
   transform: require('vinyl-transform'),
-  exorcist: require('exorcist'),
   source: require('vinyl-source-stream')
 }
 
 module.exports = function (gulp, param) {
-  var browserify = plugins.browserify({
-    cache: {},
-    packageCache: {},
-    fullPaths: true,
-    entries: [param.rootFile],
-    debug: false//gulp.isProduction ? false : true // do I need that if I use external maps?
+  var browserify = plugins.browserify(param.rootFile, {
+    debug: gulp.isProduction ? false : true
   })
 
-  browserify.transform(plugins.babelify.configure({
-    sourceMaps: false
-  }))
-
+  browserify.transform(plugins.babelify)
 
   // Browserify build and concat (We use watchify for browserify, so we dont lose in performance)
   gulp.task('browserify:watch', ['browserify:build'], function() {
@@ -34,18 +26,11 @@ module.exports = function (gulp, param) {
 
   gulp.task('browserify:build', function () {
     browserify.bundle()
-    .on('error', function(err){
+    .on('error', function(err) {
       plugins.gutil.log(plugins.gutil.colors.red(err.message))
       this.emit('end')
     })
     .pipe(plugins.source('bundle.js'))
-    .pipe(
-      gulp.isProduction ?
-      plugins.gutil.noop() :
-      plugins.transform(function () {
-        return plugins.exorcist(param.output + 'bundle.js.map')
-      })
-    )
     .pipe(gulp.dest(param.output))
   })
 }
